9cee50b582c51d02a989a9ec4e291a53ba2020bb
RomanTsagolov <rtsagolov@gmail.com>
2025-01-25 13:03:02 +0300

    DZ-3 Домашняя работа №3

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        //Домашнее задание №2. Работа с переменными
-        int number = 7;
-        String value = "Hello";
-
-    }
-}
